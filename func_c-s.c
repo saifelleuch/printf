4eaaab49bb4fe20df4ac4adff2a2c18a9a6d2b2c
louaysaidi22 <louaysaidi18@gmail.com>
2021-11-08 19:50:36 +0100

    function to print string or char

--- func_c-s.c
+++ func_c-s.c
@@ -3,28 +3,33 @@
 /**
  * pr_char - function that prints a character.
  * @x: charater to print.
+ * Return: 1
  */
-
-void pr_char(va_list x)
+int pr_char(va_list x)
 {
 	_putchar(va_arg(x, int));
+	return (1);
 }
 
 /**
  * pr_string - function that prints a string.
  * @x: string to print.
+ * Return: string length
  */
-
-void pr_string(va_list x)
+int pr_string(va_list x)
 {
 	int i = 0;
+	int l;
 	char *str;
 
+	l = _strlen(str);
 	str = va_arg(x, char *);
-	while (i < _strlen(str))
+	while (i < l)
 	{
 		_putchar(str[i]);
 		i++;
 	}
+	l--;
+	return (l);
 
 }

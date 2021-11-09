21179571e9c6074fc9aa792e68a148f5e2b56cc9
louaysaidi22 <louaysaidi18@gmail.com>
2021-11-09 12:17:07 +0100

    printf function

--- printf.c
+++ printf.c
@@ -9,39 +9,18 @@
 int _printf(const char *format, ...)
 {
 	va_list x;
-	int i = 0, j = 0, l = 0;
-	int n;
+	int n, l;
 	op_t ab[] = {
 		{"c", pr_char},
 		{"s", pr_string},
 		{NULL, NULL}
 	};
+
+	if (format == NULL)
+		return (-1);
 	va_start(x, format);
 	n = _strlen(format);
-	while (i < n)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			j = 0;
-		while (ab[j].op != NULL)
-		{
-			if (*ab[j].op == format[i])
-			{
-				l += ab[j].f(x);
-			}
-			j++;
-		}
-		i++;
-		}
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			i++;
-			l++;
-		}
-	}
-	l--;
+	l = pre_printf(format, x, ab, n);
 	va_end(x);
 	return (l);
 }
